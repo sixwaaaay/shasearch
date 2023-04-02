580b4638fb38eee440ddff0530fd0f912b39adf3
Restyled.io <commits@restyled.io>
2023-04-02 14:26:50 +0000

    Restyled by clang-format

--- src/test/java/io/github/sixwaaaay/GreetingResourceIT.java
+++ src/test/java/io/github/sixwaaaay/GreetingResourceIT.java
@@ -17,5 +17,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
+  // Execute the same tests but in packaged mode.
 }
--- src/test/java/io/github/sixwaaaay/GreetingResourceTest.java
+++ src/test/java/io/github/sixwaaaay/GreetingResourceTest.java
@@ -13,20 +13,16 @@
 
 package io.github.sixwaaaay;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
 @QuarkusTest
 public class GreetingResourceTest {
-    @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/virtual")
-                .then()
-                .statusCode(200)
-                .body(is("quarkus"));
-    }
+  @Test
+  public void testHelloEndpoint() {
+    given().when().get("/virtual").then().statusCode(200).body(is("quarkus"));
+  }
 }
